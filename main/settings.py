b0b1decca1044dcafc1e1bb0c285b6f7b9d43452
janaa7 <J.Abd2357@nu.edu.eg>
2026-09-18 01:20:02 +0300

    Update frontend CORS origins

--- main/settings.py
+++ main/settings.py
@@ -77,6 +77,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://localhost:5174',
+        'http://127.0.0.1:5174',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -89,6 +91,8 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
 ]
 
 
